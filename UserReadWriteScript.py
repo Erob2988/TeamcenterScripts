f42a58e9cd50a93c40047b12b2a49e3b2ea96044
Robinson (EXT) <pbxg9v@splm.siemens.com>
2020-01-14 08:20:07 -0500

    First Commit

---
+++ UserReadWriteScript.py
@@ -0,0 +1,13 @@
+
+
+inputFile = open("C:/Users/pbxg9v/Documents/Make_User_011320",'r')
+outputFile = open("C:/Users/pbxg9v/Documents/User_Update_011320", 'w')
+#user id starts at charater 48
+
+for line in inputFile:
+    password = line.find("-password=")
+    #print("\""+ line[48:password-1] + "\"")
+    outputFile.write("|" + line[47:password-1] + "||||licenselevel|consumer|update\n")
+
+inputFile.close()
+outputFile.close()
